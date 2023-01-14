186ff94223f74193bf5ac65c61c704f9f41cb248
rizal123 <khoerulrizal@cards.co.id>
2023-01-14 20:39:24 +0700

    feat: login logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -21,8 +21,34 @@ const register = async (req, res) => {
       const savedUser = await newUser.save();
       res.status(201).json(savedUser);
    } catch (error) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({ error: error.message });
    }
 };
 
-export { register };
+// LOGIN
+const login = async (req, res) => {
+   try {
+      const { email, userName, password } = req.body;
+
+      // Get user by username or email
+      const user = await User.findOne({ $or: [{ email, userName }] });
+      if (!user) {
+         return res.status(400).json({ message: "User does not exist. " });
+      }
+
+      // Matching password from input to database
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+         return res.status(400).json({ message: "Password does not match. " });
+      }
+
+      // Create token for login
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      delete user.password;
+      res.status(200).json({ token, user });
+   } catch (error) {
+      res.status(500).json({ error: error.message });
+   }
+};
+
+export { register, login };
